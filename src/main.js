baac0599738a8c0a923d062b897f27fe4715af24
James Harris <wopian@wopian.me>
2017-07-07 01:00:07 +0100

    feat: use post ids and interval time from env

--- src/main.js
+++ src/main.js
@@ -6,7 +6,14 @@ import duration from 'humanize-duration'
 import { version } from '../package'
 import { env } from './env'
 
-const posts = [8716591, 8716592]
+moment.relativeTimeThreshold('ss', 3)
+moment.relativeTimeThreshold('s', 59)
+moment.relativeTimeThreshold('m', 59)
+moment.relativeTimeThreshold('h', 23)
+moment.relativeTimeRounding('ss', 3)
+moment.relativeTimeRounding('s', 59)
+moment.relativeTimeRounding('m', 59)
+moment.relativeTimeRounding('h', 23)
 
 const baseUrl = 'https://kitsu.io/api'
 const auth = new OAuth2({
@@ -47,7 +54,7 @@ const main = async (ora) => {
 
   Kitsu.headers['Authorization'] = `Bearer ${accessToken}`
 
-  for (let post of await posts) {
+  for (let post of await env.POSTS) {
     ora.text = await 'Getting comments'
 
     let response = await Kitsu.findAll('comment', {
@@ -59,44 +66,60 @@ const main = async (ora) => {
     })
 
     for (let test of await response) {
-      if (test.content === env.CONTENT && test.user.name === env.USERNAME) {
+      if (test.content.slice(0, 77) === env.CONTENT.slice(0, 77) && test.user.name === env.USERNAME) {
         ora.text = await 'Deleting comment'
 
         await Kitsu.destroy('comment', test.id)
         .catch(err => console.error(err))
 
-        ora.text = await 'Posting comment'
+        if (await moment(env.FINISH).diff(moment()) > 0) {
+          ora.text = await 'Posting comment'
 
-        await Kitsu.create('comment', {
-          content: env.CONTENT,
-          user: { id: test.user.id },
-          post: { id: await post }
-        })
-        .catch(err => console.error(err))
+          await Kitsu.create('comment', {
+            content: env.CONTENT + await moment().to(await moment(env.FINISH)),
+            user: { id: test.user.id },
+            post: { id: await post }
+          })
+          .catch(err => console.error(err))
+
+          // Log actions
+          await ora.stop()
+          await console.log(`\nComment: ${test.id}`)
+          await console.log(`Content: ${test.content}`)
+          await console.log(`User: ${test.user.name}`)
+          await console.log(`Post: ${test.post.id}`)
+          await ora.start()
+        } else {
+          await Kitsu.create('comment', {
+            content: 'Voting has closed',
+            user: { id: test.user.id },
+            post: { id: await post }
+          })
+          .catch(err => console.error(err))
+        }
+
         ora.text = await 'Done'
-
-        // Log actions
-        await ora.stop()
-        await console.log(`\nComment: ${test.id}`)
-        await console.log(`Content: ${test.content}`)
-        await console.log(`User: ${test.user.name}`)
-        await console.log(`Post: ${test.post.id}`)
-        await ora.start()
       }
     }
   }
 }
 
 (() => {
+  /*
+  console.log(env.CONTENT + moment(env.FINISH).fromNow())
+  */
+
   const ora = Ora({ color: 'yellow', text: 'Starting' }).start()
-  let next = moment().add(30, 'm')
-  let now = duration(next.diff(moment()), { delimiter: ' and ', round: true })
+
+  let next = moment().add(env.INTERVAL[0], env.INTERVAL[1])
+  let now = duration(next.diff(moment()), { delimiter: ' ', round: true })
+
   setInterval(async () => {
     if (next.diff(await moment()) >= -50) {
-      now = await duration(next.diff(await moment()), { delimiter: ' and ', round: true })
+      now = await duration(next.diff(await moment()), { delimiter: ' ', round: true })
       ora.text = await `Bumping in ${now}`
     } else {
-      next = await moment().add(30, 'm')
+      next = await moment().add(env.INTERVAL[0], env.INTERVAL[1])
       await main(ora)
     }
   }, 1000)
